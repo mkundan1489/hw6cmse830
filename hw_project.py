4e07a7c1e7486cd3f4d3d538ae11459f54e6f0c7
mkundan1489 <114367938+mkundan1489@users.noreply.github.com>
2022-10-16 17:20:00 -0400

    Add files via upload

---
+++ hw_project.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 16 10:10:25 2022
+
+@author: kundan
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.utils import resample
+#from imblearn.over_sampling import SMOTENC,RandomOverSampler,KMeansSMOTE
+from sklearn.impute import KNNImputer
+from sklearn.preprocessing import LabelEncoder
+import streamlit as st
+sns.set()
+
+
+data  = pd.read_csv('hypothyroid.csv')
+data = data.drop(['TBG'], axis=1)
+data = data.drop(['TSH_measured','T3_measured','TT4_measured','T4U_measured','FTI_measured','TBG_measured'], axis=1)
+for column in data.columns:
+    count = data[column][data[column]=='?'].count()
+    if count!=0:
+        data[column] = data[column].replace('?',np.nan)    
+        
+# We can map the categorical values like below:
+data['sex'] = data['sex'].map({'F' : 0, 'M' : 1})
+
+# except for 'Sex' column all the other columns with two categorical data have same value 'f' and 't'.
+# so instead of mapping indvidually, let's do a smarter work
+for column in data.columns:
+    if  len(data[column].unique())==2:
+        data[column] = data[column].map({'f' : 0, 't' : 1})
+        
+# this will map all the rest of the columns as we require. Now there are handful of column left with more than 2 categories. 
+data = pd.get_dummies(data, columns=['referral_source'])
+
+lblEn = LabelEncoder()
+
+data['Class'] =lblEn.fit_transform(data['Class'])
+
+imputer=KNNImputer(n_neighbors=3, weights='uniform',missing_values=np.nan)
+new_array=imputer.fit_transform(data) # impute the missing values
+    # convert the nd-array returned in the step above to a Dataframe
+new_data=pd.DataFrame(data=np.round(new_array), columns=data.columns)
+
+final_data = new_data[new_data['age'] <= 100]
+
+fig=sns.pairplot(final_data, vars=['age', 'Class'])
+st.pyplot(fig)
+
+
+
+
